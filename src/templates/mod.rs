3e3e681c7922f0a70c87b038b44220a0e29d37ac
Johnathan <johnathan.stark42@gmail.com>
2025-06-23 21:54:05 -0300

    init template.rs

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,10 +1,14 @@
-use std::collections::HashMap;
+use crate::models::template::CreateTemplateResponse;
 use reqwest::{Client, Error};
 use serde_json::Value;
-use crate::models::template::CreateTemplateResponse;
+use std::collections::HashMap;
+use serde::Deserialize;
 
 pub async fn get_gupshup_templates(token: &str, app_id: &str) -> Result<Value, Error> {
-    let url = format!("https://partner.gupshup.io/partner/app/{}/templates", app_id);
+    let url = format!(
+        "https://partner.gupshup.io/partner/app/{}/templates",
+        app_id
+    );
 
     let client = Client::new();
 
@@ -19,7 +23,64 @@ pub async fn get_gupshup_templates(token: &str, app_id: &str) -> Result<Value, E
 
     Ok(resp)
 }
+#[derive(Debug, Deserialize)]
+pub  struct Res 
+{
+    status:String,
+    messageId: String
+}
 
+
+pub async fn send_template_message(
+    partner_token: &str,
+    app_id: &str,
+    source_number: &str,
+    destination_number: &str,
+    template_id: &str,
+    app_name: &str,
+    params: Vec<String>,
+    message: Option<&str>,         // ✅ agora opcional
+    postback_texts: Option<&str>,  // ✅ agora opcional (deve ser um JSON válido!)
+) -> Result<String, Error> {
+    let client = Client::new();
+
+    let url = format!(
+        "https://partner.gupshup.io/partner/app/{}/template/msg",
+        app_id
+    );
+
+    let params_json = serde_json::to_string(&params).unwrap();
+    let template_str = format!(r#"{{"id":"{}","params":{}}}"#, template_id, params_json);
+
+    let mut form = HashMap::new();
+    form.insert("channel", "whatsapp");
+    form.insert("source", source_number);
+    form.insert("sandbox", "false");
+    form.insert("destination", destination_number);
+    form.insert("template", &template_str);
+    form.insert("src.name", app_name);
+
+    if let Some(msg) = message {
+        form.insert("message", msg);
+    }
+
+    if let Some(postbacks) = postback_texts {
+        form.insert("postbackTexts", postbacks); // deve estar em JSON: `[{"index":1,"text":"Oi"}]`
+    }
+
+    let res = client
+        .post(&url)
+        .header("Authorization", partner_token)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .form(&form)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Res>()
+        .await?;
+
+    Ok(res.messageId)
+}
 
 pub async fn create_gupshup_text_template(
     token: &str,
@@ -36,7 +97,10 @@ pub async fn create_gupshup_text_template(
     allow_category_change: Option<bool>,
     enable_sample: Option<bool>,
 ) -> Result<CreateTemplateResponse, Error> {
-    let url = format!("https://partner.gupshup.io/partner/app/{}/templates", app_id);
+    let url = format!(
+        "https://partner.gupshup.io/partner/app/{}/templates",
+        app_id
+    );
     let client = Client::new();
 
     let mut params = HashMap::new();
